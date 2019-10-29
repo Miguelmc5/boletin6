333a8bab62cb7c1de48c574fcceea1bfe18f96fd
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-29 10:54:26 +0100

    6_1

---
+++ boletin6_1/Boletin6_1.java
@@ -0,0 +1,26 @@
+package boletin6_1;
+import java.util.*;
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class Boletin6_1 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    Scanner sc=new Scanner(System.in);
+    int numero;
+    
+        System.out.println("Dime un número:");
+    numero=sc.nextInt();
+    
+    if (numero<0){
+        
+    }
+    else
+        System.out.println("Es positivo");
+    }
+    
+}
